5617a690452258730e652f19d6cbd4ead2f0a695
justdheja <justdheja@gmail.com>
2022-06-01 14:35:03 +0700

    cors

--- app/routes/user.routes.js
+++ app/routes/user.routes.js
@@ -4,7 +4,7 @@ const controller = require('../controllers/user.controller');
 
 module.exports = function (app) {
 	app.use(function (req, res, next) {
-		res.header('Access-Control-Allow-Origin', process.env.FRONT_ORIGIN);
+		res.header('Access-Control-Allow-Origin', "*");
 		res.header(
 			'Access-Control-Allow-Headers',
 			'x-access-token, Origin, Content-Type, Accept'
