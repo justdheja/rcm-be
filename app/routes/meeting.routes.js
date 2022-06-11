a3ecebaaca7ef1ec4660dc998a7f34f9d9475104
justdheja <justdheja@gmail.com>
2022-06-11 17:21:24 +0700

    meeting record

--- app/routes/meeting.routes.js
+++ app/routes/meeting.routes.js
@@ -14,7 +14,15 @@ module.exports = (app) => {
 
 	app.post('/api/meeting', [verifyToken], controller.createMeeting);
 	app.get('/api/meeting/:project_id', [verifyToken], controller.getAllMeetings);
-	app.get('/api/meeting/delete/:meeting_id', [verifyToken], controller.deleteMeeting)
-	app.get('/api/meeting/detail/:meeting_id', [verifyToken], controller.getMeetingDetail)
-	app.post('/api/meeting/:meeting_id', [verifyToken], controller.addRecordLink)
+	app.get(
+		'/api/meeting/delete/:meeting_id',
+		[verifyToken],
+		controller.deleteMeeting
+	);
+	app.get(
+		'/api/meeting/detail/:meeting_id',
+		[verifyToken],
+		controller.getMeetingDetail
+	);
+	app.post('/api/meeting/:meeting_id/record', [verifyToken], controller.addRecordLink);
 };
